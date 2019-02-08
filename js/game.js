366f058f5444c8ee3b9073622f52dd86e3f735fc
mike4747 <mike4747@oh.rr.com>
2019-02-08 17:43:08 -0500

    final submission

--- js/game.js
+++ js/game.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
 
+    // display the Press any Key to Start a Game message
+    document.getElementById("commentary").style.color = "#007bff";
+    document.getElementById("commentary").innerHTML = "Press any Key to Start a Game!";
+
     // initialize an array with all the letters in it to display on the page
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    // display the Press any Key to Start a Game message
-    document.getElementById("commentary").style.color = "blue";
-    document.getElementById("commentary").innerHTML = "Press any Key to Start a Game!";
-
-    var letterH = document.getElementById("letterList");
+    // create a span element for each of the above letters, give each an appropriate id tag and have them display in the browser in upper case... plus add a line break at the halfway point
+    var letterDiv = document.getElementById("letterList");
     for (var i = 0; i < letters.length; i++) {
         if (i == 13) {
             var newLetterSpan = document.createElement("br");
-            letterH.appendChild(newLetterSpan);
+            letterDiv.appendChild(newLetterSpan);
         }
         var newLetterSpan = document.createElement("span");
         newLetterSpan.setAttribute("id", letters[i]);
         newLetterSpan.textContent = letters[i].toUpperCase();
-        letterH.appendChild(newLetterSpan);
+        letterDiv.appendChild(newLetterSpan);
     }
 
     // start all the housekeeping items for the game to start
@@ -30,73 +31,49 @@ $(document).ready(function () {
     document.getElementById("hung").innerHTML = hung;
 
     var variousWords = {
-        "fruitVeg": [
-            "apple",
-            "apricot",
-            "asparagus",
-            "avocado",
-            "banana",
-            "broccoli",
-            "cabbage",
-            "cantaloupe",
-            "carrot",
-            "celery",
-            "coconut",
-            "cucumber",
-            "eggplant",
-            "fennel",
-            "guava",
-            "grape",
-            "jicama",
-            "kohlrabi",
-            "lettuce",
-            "mango",
-            "onion",
-            "orange",
-            "papaya",
-            "parsnip",
-            "peach",
-            "pineapple",
-            "potato",
-            "pumpkin",
-            "radish",
-            "spinach",
-            "tomato",
-            "turnip",
-            "zucchini"
-        ],
+        // assign all the various random word options to this array
+        "fruitVeg": ["apple", "apricot", "artichoke", "arugula", "asparagus", "avocado", "banana", "broccoli", "cabbage", "cantaloupe", "carrot", "celery", "coconut", "cucumber", "durian", "eggplant", "fennel", "guava", "grape", "grapefruit", "jicama", "kohlrabi", "lemon", "lettuce", "mango", "onion", "orange", "papaya", "parsnip", "peach", "pineapple", "pomegranate", "potato", "pumpkin", "radish", "spinach", "tomato", "turnip", "zucchini"],
 
         generateWord: function () {
             return this.fruitVeg[Math.floor(Math.random() * this.fruitVeg.length)];
         }
     };
 
+    // show the welcome text, but include a delay based upon what event triggered it
+    function welcomeText(delayTime) {
+        setTimeout(function () {
+            document.getElementById("commentary").style.color = "green";
+            document.getElementById("commentary").innerHTML = "Welcome to 'Fruit and Vegetable' Hangman!";
+        }, delayTime);
+    }
+
     function wonGame(word) {
+        document.getElementById("wonSound").play();
         document.getElementById("commentary").style.color = "green";
         document.getElementById("commentary").innerHTML = "You've WON the game! (" + word + ")";
-        document.getElementById("wonSound").play();
         wins++;
         document.getElementById("wins").innerHTML = wins;
+        welcomeText(1500);
         startGame();
     }
 
     function lostGame(word) {
+        document.getElementById("hungSound").play();
         document.getElementById("commentary").style.color = "red";
         document.getElementById("commentary").innerHTML = "You've been HUNG! (" + word + ")";
         hung++;
         document.getElementById("hung").innerHTML = hung;
+        welcomeText(2500);
         startGame();
     }
 
     document.onkeyup = function () {
-        document.getElementById("commentary").style.color = "green";
-        document.getElementById("commentary").innerHTML = "Welcome to 'Fruit and Vegetable' Hangman!";
+        welcomeText(0);
         startGame();
     }
 
     // define startGame function
     function startGame() {
-
         // re-initialize an array with all the letters in it for the game logic
         var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -104,14 +81,11 @@ $(document).ready(function () {
         var randomWord = variousWords.generateWord();
         var randomWordLength = randomWord.length;
 
-        // show the random word while still in the testing stage
-        // document.getElementById("rWord").innerHTML = randomWord;
-
-        // show the empty gallows pic
-        document.getElementById("hangmanImage").src = "images/hangman_00.png";
-
-        // clear the commentary text
-        // document.getElementById("commentary").innerHTML = "";
+        // show the empty gallows pic after a brief delay
+        setTimeout(function () {
+            document.getElementById("hangmanImage").alt = "Empty Gallows";
+            document.getElementById("hangmanImage").src = "images/hangman_00.png";
+        }, 1500);
 
         // make all the letters show as active
         letters.forEach(letterFunction);
@@ -128,7 +102,7 @@ $(document).ready(function () {
         // initialize, format the image arrangement variable, then display it in the browser
         var formattedImageCode = "";
         for (var i = 0; i < randomWord.length; i++) {
-            formattedImageCode += '<img class="img-fluid mx-2 my-5" src="images/blank.png">';
+            formattedImageCode += '<img class="img-fluid mx-2 my-5" src="images/blank.png" alt="blank">';
         }
         document.getElementById("theWord").innerHTML = formattedImageCode;
 
@@ -169,30 +143,38 @@ $(document).ready(function () {
                             // see if the current letter is part of randomWord
                             if (correctGuessLetters.indexOf(randomWord[i]) > -1) {
                                 // the letter just guessed is part of randomWord
-                                formattedImageCode += '<img class="img-fluid mx-2 my-5" src="images/' + randomWord[i] + '.png">';
+                                formattedImageCode += '<img class="img-fluid mx-2 my-5" src="images/' + randomWord[i] + '.png" alt="' + randomWord[i] + '">';
                                 letterCounter++;
                             } else {
                                 // the letter just guessed is NOT part of randomWord
-                                formattedImageCode += '<img class="img-fluid mx-2 my-5" src="images/blank.png">';
+                                formattedImageCode += '<img class="img-fluid mx-2 my-5" src="images/blank.png" alt="blank">';
                             }
                         }
                         // display the new formattedImageCode code
                         document.getElementById("theWord").innerHTML = formattedImageCode;
 
                         // let the player know their letter selection has already been used
-                        document.getElementById("commentary").style.color = "blue";
+                        if (letterCounter < randomWordLength) {
+                            document.getElementById("correctSound").play();
+                        }
+                        document.getElementById("commentary").style.color = "#007bff";
                         document.getElementById("commentary").innerHTML = "'" + keyPressed.toUpperCase() + "' is a correct guess!";
                     } else {
                         // since the letter was not part of randomWord, increment the misses counter, update the 'Misses Remaining' counter on the page and update the hangman image
-                        document.getElementById("commentary").style.color = "red";
-                        document.getElementById("commentary").innerHTML = "'" + keyPressed.toUpperCase() + "' is NOT correct!";
 
                         // increment the misses counter and update the hanman image
                         misses++;
+                        document.getElementById("hangmanImage").alt = misses + " Misses";
                         document.getElementById("hangmanImage").src = "images/hangman_0" + misses + ".png";
 
                         // update the 'Misses Remaining' counter on the page
                         document.getElementById("missesRemaining").innerHTML = missLimit - misses;
+
+                        if (misses < missLimit) {
+                            document.getElementById("wrongSound").play();
+                        }
+                        document.getElementById("commentary").style.color = "red";
+                        document.getElementById("commentary").innerHTML = "'" + keyPressed.toUpperCase() + "' is NOT correct!";
                     }
 
                     // style the select letter to appear as inactive in the browser
@@ -202,14 +184,16 @@ $(document).ready(function () {
                     letters.splice(indexToRemove, 1);
 
                 } else {
+                    document.getElementById("wrongSound").play();
                     document.getElementById("commentary").style.color = "black";
                     document.getElementById("commentary").innerHTML = "'" + keyPressed.toUpperCase() + "' has already been used.";
                 }
             } else {
+                document.getElementById("wrongSound").play();
                 document.getElementById("commentary").style.color = "red";
                 document.getElementById("commentary").innerHTML = "Only the letter keys are valid inputs when playing Hangman";
             }
-            
+
             if (misses == missLimit) {
                 // the player has been hung, so run the lostGame function
                 lostGame(randomWord);
